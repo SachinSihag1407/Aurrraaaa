8ed85a7f9d7fd98ea338d9069d37e17600a27025
sachinsihag <sachinsihag1407@gmail.com>
2025-08-01 01:58:32 +0530

    add register controller and fixes bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,81 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { AsyncHandler } from "../utils/Asynchandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
-const registerUser = AsyncHandler (async(req,res)=>{
-    // res.status(200).json(new ApiResponse(200,"Sachu Don"))
-    // another way 
-    res.status(200).json({
-        message : "Sachu Don"
+const registerUser = AsyncHandler(async (req, res) => {
+    // // res.status(200).json(new ApiResponse(200,"Sachu Don"))
+    // // another way 
+    // res.status(200).json({
+    //     message : "Sachu Don"
+    // })
+
+    // get the user detail from the frontend 
+    //validaton
+    //check if the user is alredy exists or not
+    //check for avatar and cover Image
+    // upload on cloudinary
+    // now create a user object and make entry in db
+    // remove the password and refresh token
+    // check response
+    // return response
+
+    const { fullName, email, username, password } = req.body;
+    // ab hme yha ek ek krke sb liye validation check krni pdegi is se bdiya eksath kr lete h 
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+        // iska mtlb h ki agr mil gya to trim kr do and fir bhi empty h to true return 
+    ) {
+        throw new ApiError(400, "all fields are required to fill")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
-}) 
-     // isme hmne message pass kiya  h 
+
+    if (existedUser) {
+        throw new ApiError(409, "The User is already existed ")
+    }
+
+    const avatarLoacalPath = req.files?.avatar[0]?.path;
+    const coverImageLoacalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLoacalPath) {
+        throw new ApiError(400, "Avatar fileis required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLoacalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLoacalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar fileis required")
+    }
+
+    const user = User.create({
+        username: username.toLower(),
+        fullName,
+        email,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password,
+    })
+
+    const createdUser = User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+
+
+
+
+})
+// isme hmne message pass kiya  h 
 export default registerUser;
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -19,3 +19,5 @@ const upload = multer(
 
     }
 )
+
+export {upload};
